b5acbef8f03095e773fd288b305fd65554caef22
Ubuntu <ubuntu@ip-172-31-36-37.ec2.internal>
2024-07-28 17:48:45 +0000

    query data from db using db.QueryRow

--- main.go
+++ main.go
@@ -17,14 +17,25 @@ type Product struct {
 	if err != nil{
 		log.Fatal(err)
 	}
-err = db.Ping();
-if err != nil{
-	log.Fatal(err)
-}
-CreateProductTable(db)
-product := Product{"Book",15.55,true}
-pk := insertProduct(db,product)
-fmt.Println("ID : %d",pk)
+    err = db.Ping();
+    if err != nil{
+	  log.Fatal(err)
+    }
+    CreateProductTable(db)
+    product := Product{"Table ",200,true}
+    pk := InsertProduct(db,product)
+    fmt.Println("ID : %d",pk)
+	var name  string
+	var price float64
+	var available bool
+	query := "SELECT name ,available, price FROM product WHERE id = $1"
+    errr := db.QueryRow(query,pk).Scan(&name, &available, &price)
+	if errr != nil{
+		log.Fatal(errr)
+	}
+	fmt.Println("name : %v",name)
+	fmt.Println("price : %v",price)
+	fmt.Println("available : %v",available)
  }
 
 func CreateProductTable(db *sql.DB ){
@@ -35,19 +46,23 @@ func CreateProductTable(db *sql.DB ){
 	available BOOLEAN,
 	created timestamp DEFAULT NOW()
 	)`
- _, err :=  db.Exec(query)
- if err != nil{
+    _, err :=  db.Exec(query)
+    if err != nil{
 	log.Fatal(err)
-}
+     }
 } 
+// This function insert  data from Database 
 
-func insertProduct(db *sql.DB ,product Product ) int{
+func InsertProduct(db *sql.DB ,product Product ) int{
    query := ` INSERT INTO product (name,price,available)
    VALUES ($1, $2,$3 ) RETURNING id`
- var pk int   
+   var pk int   
    err := db.QueryRow(query,product.Name,product.Price,product.Available).Scan(&pk)
    if err != nil{
 	log.Fatal(err)
+   }
+   return pk
 }
-return pk
-}
+
+
+
